a3e914db9331311a75df1808b3d68b93ea289e64
Khasanov Petr <petr.khasanov@metalamp.io>
2026-07-26 13:09:26 +0300

    build(license): inventory third-party components and ship notices with the game
    Signed-off-by: Khasanov Petr <petr.khasanov@metalamp.io>

---
+++ poc/src/license.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace engine {
+
+constexpr const char* LICENSE_ATTRIBUTION = "POWERED BY LIKE NES";
+constexpr const char* LICENSE_TERMS = "FREE SOFTWARE MIT OR APACHE 2 0";
+
+} // namespace engine
